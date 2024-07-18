1c8b27f3344e1de3727616ee92ccec6b9b8778e2
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-07-18 15:49:25 +0800

    :rocket: added status to api response

--- apis/views.py
+++ apis/views.py
@@ -30,7 +30,7 @@ def signup(request):
         user.set_password(request.data['password'])
         user.save()
         token = Token.objects.create(user=user)
-        return Response({'token': token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+        return Response({'success': True, 'token': token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
